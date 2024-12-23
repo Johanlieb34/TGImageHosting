28a423f42595aed1dbe620d006f64bf91900d755
꧁༺ӄɨռɢ ʝօɦǟռ༻꧂ <godswillosamaye810@gmail.com>
2024-12-23 19:05:18 +0100

    Update cofig.py

--- cofig.py
+++ cofig.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.environ.get("API_ID")
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = os.environ.get("21060976")
+API_HASH = os.environ.get("b8b9e236dd578d07621f5a8174a891d9")
+BOT_TOKEN = os.environ.get("7560514089:AAHY5d0q7GoEQMvN_TbbezTwtFlLuiVsF2Q")
